0de37cd046eb58d3cb85a3563f277687d3677058
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-12-19 11:44:34 +0530

    password valiudation added

--- UserAuth/parameter.py
+++ UserAuth/parameter.py
@@ -19,9 +19,9 @@ class UserAuthsParameter(UserAuthSchema):
         if UserAuth.query.filter_by(email=data).first():
             raise base_fields.ValidationError('Email already exists')
 
-    # @validates('password')
-    # def validate_password(self, data, **kwargs):
-    #     match_obj = re.match('^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()-_+=])[A-Za-z\d!@#$%^&*()-_+=]{4,}$', data)
-    #     if not match_obj:
-    #         raise base_fields.ValidationError('Password must be at least 4 characters long and contain at least one \
-    #          uppercase letter, one lowercase letter, one number, and one special character')
+    @validates('password')
+    def validate_password(self, data, **kwargs):
+        match_obj = re.match('^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()-_+=])[A-Za-z\d!@#$%^&*()-_+=]{4,}$', data)
+        if not match_obj:
+            raise base_fields.ValidationError('Password must be at least 4 characters long and contain at least one \
+             uppercase letter, one lowercase letter, one number, and one special character')
