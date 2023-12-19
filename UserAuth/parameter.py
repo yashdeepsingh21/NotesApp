279786e797760cbc4a7c62a52e43c339691c46a5
Yashdeep Singh <singhyashdeep720@gmail.com>
2023-12-19 11:40:15 +0530

    password validation added

--- UserAuth/parameter.py
+++ UserAuth/parameter.py
@@ -2,7 +2,7 @@ from marshmallow import validates, fields as base_fields, post_load, validate
 from extensions import ma
 from UserAuth.schema import UserAuthSchema
 from UserAuth.models import UserAuth
-
+import re
 
 class UserAuthsParameter(UserAuthSchema):
     user_name = base_fields.String(required=True, validate=validate.Length(min=2, max=255),
@@ -18,3 +18,10 @@ class UserAuthsParameter(UserAuthSchema):
     def validate_email(self, data, **kwargs):
         if UserAuth.query.filter_by(email=data).first():
             raise base_fields.ValidationError('Email already exists')
+
+    @validates('password')
+    def validate_password(self, data, **kwargs):
+        match_obj = re.match('^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()-_+=])[A-Za-z\d!@#$%^&*()-_+=]{4,}$', data)
+        if not match_obj:
+            raise base_fields.ValidationError('Password must be at least 4 characters long and contain at least one \
+             uppercase letter, one lowercase letter, one number, and one special character')
